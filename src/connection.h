45ca1dc34c907f68ba04b3fc7233604051f07bc4
litao.sre <litao.sre@bytedance.com>
2019-02-25 01:06:46 +0800

    Refactor connection with pollable
    Change-Id: I05b2b27eb883da135470b515882a188c6f2acbe8

--- src/connection.h
+++ src/connection.h
@@ -5,27 +5,18 @@
 #include <memory>
 
 #include "handler.h"
+#include "pollable.h"
 
 namespace hera {
 
-class Connection {
+class Connection : public Pollable {
  public:
   Connection(int efd, int fd, sockaddr_in addr, HandlerPtr handler);
   virtual ~Connection();
 
-  Connection(const Connection&) = delete;
-  Connection(Connection&&) = delete;
-  Connection& operator=(const Connection&) = delete;
-  Connection& operator=(Connection&&) = delete;
+  bool OnRead() override;
+  bool OnWrite() override;
 
-  bool EnableRead();
-  bool EnableWrite();
-  bool DisableWrite();
-
-  bool OnRead();
-  bool OnWrite();
-
-  int fd() const { return fd_; }
   const char* addr_id() const { return addr_id_; }
   BufferPtr read_buf() const { return read_buf_; }
   BufferPtr write_buf() const { return write_buf_; }
@@ -36,12 +27,6 @@ class Connection {
   static const size_t kMinBufCap;
   static const size_t kBufExpandThreshold;
 
-  bool AddEvent(int events);
-  bool DelEvent(int events);
-
-  int efd_;
-  int fd_;
-  int events_;
   sockaddr_in addr_;
   char* addr_id_;
   HandlerPtr handler_;
